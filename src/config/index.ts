6a78b867600c0bc8bebacb7c2d5673f98b37f43b
19khaled88 <kkhaled88hasan@gmail.com>
2023-10-11 08:25:04 +0600

    back with api gateway

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,11 +6,14 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 const envVarsZodSchema = z.object({
     NODE_ENV: z.string(),
+    REDIS_URL: z.string(),
     PORT: z
         .string()
         .default('3030')
         .refine((val) => Number(val)),
-    JWT_SECRET: z.string()
+    JWT_SECRET: z.string(),
+    AUTH_SERVICE_URL: z.string(),
+    CORE_SERVICE_URL: z.string()
 });
 
 const envVars = envVarsZodSchema.parse(process.env);
@@ -18,7 +21,12 @@ const envVars = envVarsZodSchema.parse(process.env);
 export default {
     env: envVars.NODE_ENV,
     port: envVars.PORT,
+    redis: {
+        url: envVars.REDIS_URL
+    },
     jwt: {
         secret: envVars.JWT_SECRET
-    }
+    },
+    authserver_url: envVars.AUTH_SERVICE_URL,
+    coreserver_url: envVars.CORE_SERVICE_URL
 };
